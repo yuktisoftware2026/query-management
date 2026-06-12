84c77e205a6c74a5d66b6229f2a37f509c4e74d4
YuktiSoftware2026 <yuktisoftware2026@gmail.com>
2026-06-12 14:15:34 +0530

    Swagger info fields added

--- src/main/java/in/yuktisoftwares/query_management/config/OpenApiConfig.java
+++ src/main/java/in/yuktisoftwares/query_management/config/OpenApiConfig.java
@@ -1,6 +1,7 @@
 package in.yuktisoftwares.query_management.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +11,16 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        Contact contact = new Contact();
+        contact.email("contact@yuktisoftware.com");
+        contact.name("Yukti Software");
+        contact.url("https://www.yuktisoftwares.com");
         return new OpenAPI()
                 .info(new Info()
                         .title("Query Management API")
                         .version("1.0")
-                        .description("API documentation for Query Management"));
+                        .description("API documentation for Query Management")
+                        .contact(contact)
+                );
     }
 }
